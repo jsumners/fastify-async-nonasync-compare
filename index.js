4d3dfa295f89fc4368f51add3f5bf4ebed433812
James Sumners <james.sumners@gmail.com>
2018-03-26 10:05:48 -0400

    Simulate waiting on slow external operation

--- index.js
+++ index.js
@@ -3,11 +3,17 @@
 const fastify = require('fastify')()
 
 fastify.get('/regular', function (req, reply) {
-  reply.send({hello: 'world'})
+  setTimeout(() => reply.send({hello: 'world'}), 100)
 })
 
+async function dbQuery () {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve({hello: 'world'}), 100)
+  })
+}
+
 fastify.get('/async', async function (req, reply) {
-  return {hello: 'world'}
+  return await dbQuery()
 })
 
 fastify.listen(2000, (err) => {
